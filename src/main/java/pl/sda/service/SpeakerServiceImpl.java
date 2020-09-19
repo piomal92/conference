3f0e51815c60b91c0b824cafbc7eacd54580a7a7
piotr <piomal3@gmail.com>
2020-09-19 12:32:54 +0200

    commit - constructor injection

--- src/main/java/pl/sda/service/SpeakerServiceImpl.java
+++ src/main/java/pl/sda/service/SpeakerServiceImpl.java
@@ -10,12 +10,21 @@ public class SpeakerServiceImpl implements SpeakerService {
 
     private SpeakerRepository speakerRepository;
 
+    public SpeakerServiceImpl(SpeakerRepository speakerRepository) {
+        System.out.println("Constructon injection");
+        this.speakerRepository = speakerRepository;
+    }
+    public SpeakerServiceImpl(){
+
+    }
+
     @Override
     public List<Speaker> findAll(){
         return speakerRepository.findAll();
     }
 
     public void setSpeakerRepository(SpeakerRepository speakerRepository) {
+        System.out.println("Setter injection");
         this.speakerRepository = speakerRepository;
     }
 }
